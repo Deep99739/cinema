8146d8e21368b91463559d8ffba983805ef3f96f
Deepak kumar <deepakkumar@Deepaks-MacBook-Air.local>
2024-06-19 10:49:59 +0530

    finnaly solved the api issue

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,16 +3,23 @@ import { useState, useEffect } from "react";
 export const useFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
 
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
-  //   console.log(url);
   useEffect(() => {
     async function fetchMovies() {
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data.results);
+      const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data.results);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
     }
+    
     fetchMovies();
-  }, [url]);
+  }, [apiPath, queryTerm]);
 
   return { data };
 };
